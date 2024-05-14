2a4cf83d44fd83dc67c0c4497290f18beca257cc
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-14 22:24:49 +0530

    Adding Comment to PHP

--- php/personal_submit.php
+++ php/personal_submit.php
@@ -1,6 +1,5 @@
 <?php
 
-
 //^ Database credentials
 $host = 'localhost';
 $username = 'root';
@@ -61,6 +60,7 @@ if (!$result) {
   <title>Personal Details</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <style>
+    /* for Pushing footer at the bottom */
     body {
       display: flex;
       flex-direction: column;
